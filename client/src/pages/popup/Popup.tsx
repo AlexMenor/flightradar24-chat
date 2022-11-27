ed7bb2c11a48ac243335ffa626bf13a9332437a5
Alex Menor <alex.menor@icloud.com>
2022-11-27 13:32:59 +0100

    add better url management in popup

--- client/src/pages/popup/Popup.tsx
+++ client/src/pages/popup/Popup.tsx
@@ -4,6 +4,11 @@ import { useEffect, useState } from "react";
 import { Chat } from "./Chat";
 import { trpc } from "./trpc";
 
+type FlightDetails =
+  | { status: "not-fr24" }
+  | { status: "no-flight-selected" }
+  | { status: "ok"; flightName: string; flightId: string };
+
 export default function Popup() {
   const [url, setUrl] = useState<string | null>(null);
 
@@ -19,25 +24,32 @@ export default function Popup() {
     fetchUrl().then(setUrl);
   }, []);
 
-  function getFlightDetailsFromUrl(url: string) {
+  function getFlightDetailsFromUrl(url: string): FlightDetails {
     const parsedUrl = new URL(url);
+
+    if (parsedUrl.host !== "www.flightradar24.com") {
+      return { status: "not-fr24" };
+    }
 
     const pathnameFragments = parsedUrl.pathname.split("/").filter(Boolean);
     if (pathnameFragments.length < 2) {
-      return null;
+      return { status: "no-flight-selected" };
     }
 
     const flightName = pathnameFragments[0];
     if (flightName.includes(",")) {
-      return null;
+      return { status: "no-flight-selected" };
     }
 
     const flightId = pathnameFragments[1];
 
-    return { flightName, flightId };
+    return { status: "ok", flightName, flightId };
   }
 
-  const flightDetails = url && getFlightDetailsFromUrl(url);
+  const flightDetails: FlightDetails = url
+    ? getFlightDetailsFromUrl(url)
+    : { status: "not-fr24" };
+
   const [queryClient] = useState(() => new QueryClient());
   const [wsClient] = useState(() =>
     createWSClient({ url: import.meta.env.VITE_WS_ENDPOINT })
@@ -61,14 +73,28 @@ export default function Popup() {
     <trpc.Provider client={trpcClient} queryClient={queryClient}>
       <QueryClientProvider client={queryClient}>
         <div className="h-full bg-fr-gray font-sans text-fr-light">
-          {flightDetails ? (
+          {flightDetails.status === "ok" ? (
             <Chat
               chatId={flightDetails.flightId}
               flightName={flightDetails.flightName}
             />
           ) : (
-            <div className="h-full flex items-center justify-center">
-              <p>We cannot detect flight details, please select a flight</p>
+            <div className="h-full flex items-center justify-center text-sm">
+              {flightDetails.status === "not-fr24" ? (
+                <p>
+                  This extension only works in{" "}
+                  <a
+                    className="underline"
+                    target="_blank"
+                    rel="noreferrer noopener"
+                    href="https://www.flightradar24.com"
+                  >
+                    flightradar24
+                  </a>
+                </p>
+              ) : (
+                <p>We cannot detect flight details, please select a flight</p>
+              )}
             </div>
           )}
         </div>
